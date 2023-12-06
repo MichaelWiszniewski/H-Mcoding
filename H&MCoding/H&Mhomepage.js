d171f098ac9e9b74988928f405689dd54689feb7
Michael Wiszniewski <106599604+MichaelWiszniewski@users.noreply.github.com>
2023-12-06 12:41:51 -0500

    Update H&Mhomepage.js

--- H&MCoding/H&Mhomepage.js
+++ H&MCoding/H&Mhomepage.js
@@ -1,20 +1,54 @@
+ // Here we initialize an array to store all scores
+var allScores = []; 
+
+function addScore(name, difficulty) {
+     let score = [name, difficulty];
+     allScores.push(score);
+ 
+     // Here we sort the array based on the difficulty level in descending order
+     allScores = allScores.sort((a, b) => b[1] - a[1]);     
+ }
+
+
+function updateWinners() {
+     // Here we display scores in the table
+    
+    tableBody = document.getElementById("topScorersTableBody");
+    // Here we are clearing the table body for the next element
+    while (tableBody.hasChildNodes()) {
+        tableBody.removeChild(tableBody.firstChild);
+      };
+    
+    allScores.forEach(function(score) {
+        var row = document.createElement("tr");
+        row.innerHTML = "<td>" + score[0] + "</td><td>" + score[1] + "</td>";
+        tableBody.appendChild(row);
+    });
+}
+
 // Here we have a function to get a random integer within a given range
 function randomInt(max) {
     return Math.floor(Math.random() * (max)) + 1;
 }
 
 function playGame() {
+    
+    var username = (prompt("What is your name? "));
+    while (!username || !username.trim()) {
+        username = (prompt("Please enter a valid username. What is your name? "));
+    };
+    
     // Here we get user input for the range of integers and handle possible input errors
     var range = parseInt(prompt('Enter the range of possible integers (range goes from 1 to this number): '));
     while (isNaN(range) || range <= 1) {
         range = parseInt(prompt('Please enter a valid integer greater than 1:'));
-    }
+    };
 
     // Here we get user input for the number of attempts and handle possible input errors
     var maxAttempts = parseInt(prompt('Enter the number of attempts:'));
     while (isNaN(maxAttempts) || maxAttempts <= 0) {
         maxAttempts = parseInt(prompt('Please enter a valid integer number of attempts greater than 0:'));
-    }
+    };
 
     // Here we generate a random integer within the specified range, 1 being the min and user given int being max
     var targetNumber = randomInt(range);
@@ -32,10 +66,14 @@ function playGame() {
             alert('Please enter a valid integer.');
         } else {
             if (userGuess == targetNumber) {
-                document.getElementById("output").innerHTML = 'Congratulations you have completed the MindDigits challenge\
+                document.getElementById("output").innerHTML = 'Congratulations ' + username + ' you have completed the MindDigits challenge\
                 on difficulty rating ' + difficulty.toString() + '! You guessed the correct number (' + targetNumber + ') in ' + (attempts + 1) + ' attempts.';
 
-                break;
+                addScore(username, difficulty);
+                
+                updateWinners();
+                
+                attempts = maxAttempts;
                                
             } else if ((userGuess != targetNumber) && (attempts < (maxAttempts - 1))) {
                 alert('Incorrect guess. Try again!');
@@ -45,10 +83,12 @@ function playGame() {
         }
 
         attempts++;
-    }
+    };
 
     // Here we have the message displayed if the user runs out of attempts
     if (attempts == maxAttempts) {
         document.getElementById("output").innerHTML = 'Sorry, you\'ve run out of attempts on difficulty rating ' + difficulty.toString() + '. The correct number was ' + targetNumber + '.';
     }
+
+   
 }
